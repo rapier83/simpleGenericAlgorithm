0fd756996b51b438cd753f97a6a301ae9ee7e6be
KEATON-RYZEN\rapie <rapier83@gmail.com>
2018-06-11 14:53:29 +0900

    It's work. I used `cumsum(intputVector)` is it right?

--- roulette.m
+++ roulette.m
@@ -6,18 +6,15 @@
 % ---
 
 % Input an 1-dimension numeric matrix,
-% If you don't need index number, roulette(m,n, **true**)
 
-function result = roulette(m, r, shuffle, sort) 
-    %% Section 0: - Check Arguments
+function result = roulette(m, n, varargin) 
+    Defaults = {};
+    IDX = ~cellfun('isempty', varargin);
+    Defaults(IDX) = varargin(IDX);
+    if isempty(Defaults)
+        t = 'none';
+    end
     
-    if isempty(shuffle)
-        shuffle = false;
-    end
-    if isempty(sort)
-        sort = false;
-    end
-
     %% Section 1: - Data Cleaning
     % ## All data must have an one column or row.
     % Function have return 1 where amount of `m` is 1 or 0.
@@ -37,32 +34,37 @@ function result = roulette(m, r, shuffle, sort)
     % * NaN is turned to be 0.
     m(isnan(m)) = 0;
     
-    % if shuffle is true let it do `v(randperm(length(v))`
-    m = m(randperm(length(v1)));
-
     %% Section 2: - Set Roll-Board     
     %   The boarder starts from 0 to max(m)
     %   If you need the casino roulette, input vector like [1; 1; ...];
     %   If shuffle is `ON` number will be replaced.
-        
-    board = cumsum(m);
+    switch t
+        case 'shuffle'
+            % shuffle function is`v(randperm(length(v))`
+            m = m(randperm(length(v1)));
+        case 'sort'
+            m = sort(m);
+        case 'none'
+            
+    end
+    
+    % Set boarder by culmulative summary
+    % the random number `pin` will roll roulette, and be stop 
+    % some range in where from 0 to sum of stop point
+    b = [0; cumsum(m);];
+    
+    % check is result of roulette
+    % `n` if and only if `sum(check)`
+    chk = zeros(length(m),1);
     
     %% Section 3: - Roll Dice
     %   
-    numroll = int16(rand(1,r) * sum(m)); 
-    check = zeros(length(m));
-    cursor = 1;
-    while cursor < r
-        if cursor == r
-            isin = (number(cursor) < numroll);
-        else
-            isin = all(m(cursor) >= numroll ...
-                && m(cursor + 1) < numroll);
-        end
-        if isin == 1
-            check(cursor) = check(cursor) + 1;
-            cursor = cursor + 1;
-        end
+    for i = 1:n
+        c = repmat(rand * max(b), 1, length(m) + 1)'
+        s = c < b;
+        idx = find(s~=0, 1, 'first');
+        chk(idx-1) = chk(idx-1) + 1;
     end
     
+    result = chk;
 end
